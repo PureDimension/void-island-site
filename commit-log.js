f7136c770da809b2df95da336a3e65cc7e233678
PureDimension <PureDimension@github.com>
2025-05-20 08:05:24 +0800

    chore: auto-update commit-log.js

--- commit-log.js
+++ commit-log.js
@@ -1,5 +1,11 @@
 export const logs = [
   {
+    "title": "20250514\u68a6\u65e5\u8bb0\u7d20\u6750",
+    "date": "2025-05-20 08:05:12",
+    "slug": "2025-5-20-\u68a6\u65e5\u8bb0",
+    "section": "star-dream"
+  },
+  {
     "title": "\u957f\u7ebfDDL\u4f5c\u6218\u8bb0\u5f55",
     "date": "2025-05-19 23:06:37",
     "slug": "2025-5-19-\u957f\u7ebfDDL\u4f5c\u6218\u8bb0\u5f55",
@@ -22,11 +28,5 @@ export const logs = [
     "date": "2025-05-14 03:59:39",
     "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
     "section": "ark"
-  },
-  {
-    "title": "\u03a9\u7a7a\u5c9b\u7684\u7814\u7a76\u65e5\u5fd7",
-    "date": "2025-05-14 03:59:29",
-    "slug": "2025-5-14-\u03a9\u7a7a\u5c9b\u7684\u79d8\u5bc6",
-    "section": "island-world"
   }
 ];
